6d40591a0381214bcfc1ab9dc0f105e4f9aa3f0c
Dan Bowen <dan@crucialwebstudio.com>
2020-05-26 19:47:59 -0500

    Fix issues with request validation in helper client

--- services/api/test/test_twilio_request.py
+++ services/api/test/test_twilio_request.py
@@ -12,14 +12,19 @@ class TwilioTests(unittest.TestCase):
 
     def test_validate_twilio_request(self):
         path = '/bot/ping'
-        params = {}
 
         # valid request
-        response = self.helper.twilio_request('POST', path, params)
+        response = self.helper.twilio_request('POST', path, {})
+        self.assertEqual(response.status_code, 200)
+
+        response = self.helper.twilio_request('POST', path, {'with': 'params'})
         self.assertEqual(response.status_code, 200)
 
         # invalid request
-        response = self.helper.twilio_request('POST', path, params, valid=False)
+        response = self.helper.twilio_request('POST', path, {}, valid=False)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.helper.twilio_request('POST', path, {'with': 'params'}, valid=False)
         self.assertEqual(response.status_code, 403)
 
 
