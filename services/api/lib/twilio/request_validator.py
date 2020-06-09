c8aed920d4db2945b2d7365dd70d92508cc72796
Dan Bowen <dan@crucialwebstudio.com>
2020-06-09 18:16:59 -0500

    Solve issues with API Gateway
    - use API_GATEWAY_BASE_PATH to trim stage from path prefix

--- services/api/lib/twilio/request_validator.py
+++ services/api/lib/twilio/request_validator.py
@@ -15,18 +15,44 @@ def validate_twilio_request(f):
             current_app.config['SECRETS'].TWILIO_AUTH_TOKEN
         )
 
-        # solve issues with NGROK
-        url = request.url
-        original_host = request.headers.get('X-Original-Host')
-        if original_host:
-            url_parts = urlparse(url)
-            url = urlunparse(url_parts._replace(netloc=original_host))
+        # save variables from original request as we will be making transformations on it below
+        original_url = request.url
+        original_host_header = request.headers.get('X-Original-Host')
+
+        # the url parts to be transformed
+        twilio_url_parts = urlparse(original_url)
+
+        """
+        Solve issues with NGROK
+        
+        Twilio sees: http://somedomain.ngrok.io
+        App sees:    http://localhost:5000
+        
+        So we replace the domain our app sees with the X-Original-Host header
+        """
+        if original_host_header:
+            twilio_url_parts = twilio_url_parts._replace(netloc=original_host_header)
+
+        """
+        Solve issues with API Gateway custom domains
+        
+        Twilio sees: https://custom-domain.com/bot/validate-next-alert
+        App sees:    https://custom-domain.com/{stage}/bot/validate-next-alert
+        
+        So we strip API_GATEWAY_BASE_PATH from the beginning of the path
+        """
+        api_gateway_base_path = current_app.config['API_GATEWAY_BASE_PATH']
+        if api_gateway_base_path:
+            # Strip N chars from beginning of path.
+            # +1 accounts for a leading /slash since API_GATEWAY_BASE_PATH does not have it
+            chars_to_strip = len(api_gateway_base_path) + 1
+            new_path = twilio_url_parts.path[chars_to_strip:]
+            twilio_url_parts = twilio_url_parts._replace(path=new_path)
 
         # Validate the request using its URL, POST data, and X-TWILIO-SIGNATURE header
         request_valid = twilio_client.validate_request(
-            url,
-            request.form,
-            request.headers.get('X-TWILIO-SIGNATURE', ''))
+            urlunparse(twilio_url_parts), request.form, request.headers.get('X-TWILIO-SIGNATURE', '')
+        )
 
         # Continue processing the request if it's valid, return a 403 error if it's not
         if request_valid:
