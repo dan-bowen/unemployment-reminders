efbb455d1614990ede71afbc875d2a3f8dcc9375
Dan Bowen <dan@crucialwebstudio.com>
2020-07-06 10:15:29 -0500

    Configure nose2 to discover files relative to start-dir
    - the previous code-directories setting had no effect since nose2 starts code discovery relative to start-dir

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,1 @@
-from os.path import dirname, abspath, join
-import sys
 
-# Find code directory relative to test directory
-THIS_DIR = dirname(__file__)
-CODE_DIR = abspath(join(THIS_DIR, '../src'))
-sys.path.append(CODE_DIR)
